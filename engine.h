ff446449c821aeed2c9e2dacc680af8487f20e23
Lamonin <maze.zte@gmail.com>
2022-04-12 15:13:39 +1000

    Simple commit

--- engine.h
+++ engine.h
@@ -1,12 +1,102 @@
-//
-// Created by mazez on 11.03.2022.
-//
-
 #ifndef CRPGAME_ENGINE_H
 #define CRPGAME_ENGINE_H
 
+#include <iostream>
+#include <string>
+
 namespace crpg
 {
+    class Race
+    {
+    public:
+        int strength{};
+        int agility{};
+        int intelligence{};
+        std::string name{};
+
+        Race() = default;
+    };
+
+    class Human: public Race
+    {
+    public:
+        Human() : Race()
+        {
+            name  = "Человек";
+            strength = 10;
+            agility = 10;
+            intelligence = 10;
+        }
+    };
+
+    class Orc: public Race
+    {
+    public:
+        Orc() : Race()
+        {
+            name  = "Орк";
+            strength = 10;
+            agility = 10;
+            intelligence = 10;
+        }
+    };
+
+    class Elf: public Race
+    {
+    public:
+        Elf() : Race()
+        {
+            name  = "Эльф";
+            strength = 10;
+            agility = 10;
+            intelligence = 10;
+        }
+    };
+
+    class Hero
+    {
+    private:
+        int strength{};
+        int agility{};
+        int intelligence{};
+
+        Race* race{};
+
+    public:
+        Hero() = default;
+        Hero(Hero&& rhs) noexcept { std::cout << "CPP HELL"; }
+
+        void setRace(Race* _race)
+        {
+            race = _race;
+        }
+
+        const Race* getRace()
+        {
+            return race;
+        }
+    };
+
+    class HeroBuilder
+    {
+    private:
+        Hero hero;
+
+    public:
+        HeroBuilder() = default;
+        ~HeroBuilder() = default;
+
+        void setRace(Race* _race)
+        {
+            hero.setRace(_race);
+        }
+
+        Hero&& getHero()
+        {
+            return std::move(hero);
+        }
+    };
+
 
 }
 
--- main.cpp
+++ main.cpp
@@ -8,59 +8,15 @@ int main() {
     system("chcp 65001"); //Отображение кирилицы
     system("cls");
 
-    //INITIALIZER_LIST's
-    bilist<int> list = {0,0,1,2};
-    list.cout_list();
-    list.push_back({3,4,5,6,7});
-    list.cout_list();
+    crpg::HeroBuilder builder;
 
-    //PUSH_BACK
-    list.push_back(8);
-    list.push_back(9);
-    list.push_back(0);
-    list.push_back(0);
+    builder.setRace(new crpg::Orc());
+    auto h = builder.getHero();
 
-    list.cout_list();
+    cout << h.getRace()->name << endl << "EBABOMBA" << endl;
 
-    //POP_FRONT
-    list.pop_front();
-    list.pop_front();
-
-    list.cout_list();
-
-    //POP_BACK
-    list.pop_back();
-    list.pop_back();
-
-    list.cout_list();
-
-    //ITERATOR_MAGIC
-    cout << "\nREVERSE: ";
-    std::reverse(list.begin(), list.end());
-    for (auto l : list)
-        cout << l << " ";
-
-    cout << "\nREVERSE AGAIN: ";
-    std::reverse(list.begin(), list.end());
-    for (auto it = list.begin(); it != list.end(); it++)
-        cout << *it << " ";
-
-    //ACCESS BY INDEX
-    cout << endl;
-    cout << list[0] << endl;
-    cout << list[list.size()-1] << endl;
-
-    //REMOVE BY INDEX
-    cout << endl;
-    list.removeAt(3);
-    list.removeAt(4);
-    list.removeAt(list.size()-1);
-    list.cout_list();
-
-    //CLEAR
-    list.clear();
-    cout << "\nCLEAR LIST";
-    list.cout_list();
+    builder.setRace(new crpg::Elf());
+    cout << h.getRace()->name << endl;
 
     return 0;
 }
