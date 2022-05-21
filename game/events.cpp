e8641cb4aaa90dc5052a85e81aeb6c64d180acdd
Maxim <maze.zte@gmail.com>
2022-05-21 12:40:55 +1000

    Add simple event

--- game/events.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by mazez on 18.05.2022.
-//
-
-#include "events.h"
--- game/events.h
+++ game/events.h
@@ -1,8 +1,39 @@
-//
-// Created by mazez on 18.05.2022.
-//
 
 #ifndef CRPGAME_EVENTS_H
 #define CRPGAME_EVENTS_H
+
+#include <vector>
+#include <functional>
+
+template<typename ...Args>
+class event
+{
+private:
+    using functor = std::function<void(Args...)>;
+    std::vector<functor> functors;
+
+public:
+    event() = default;
+    virtual ~event() = default;
+
+    int bind(functor f)
+    {
+        functors.push_back(f);
+        return functors.size()-1;
+    }
+
+    void unbind(int f_index)
+    {
+        if (f_index < 0 || f_index >= functors.size()) return;
+        functors.erase(functors.begin()+f_index);
+    }
+
+    void invoke(Args...params)
+    {
+        for(const auto& func:functors) func(params...);
+    }
+};
+
+
 
 #endif //CRPGAME_EVENTS_H
